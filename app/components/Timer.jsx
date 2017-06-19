a829465ea3c5dc606d1074a6c370a9f0b6c81bab
Matthew Heath <matthew.heath@hotmail.co.uk>
2017-06-19 14:53:40 +0100

    Support foundation settings

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,53 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
+var TimerForm = require('CountdownForm');
 
 var Timer = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0,
+      timerStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.timerStatus !== prevState.timerStatus) {
+      switch (this.state.timerStatus) {
+        case 'started':
+          this.handleStartTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+  },
+  handleStartTimer: function () {
+    this.timer = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }, 1000);
+  },
+  handleStatusChange: function (newTimerStatus) {
+    this.setState({timerStatus: newTimerStatus});
+  },
+
   render: function() {
+    var {count, timerStatus} = this.state;
+
     return (
-      <h4>Timer.jsx</h4>
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds = {count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+      </div>
     );
   }
 });
